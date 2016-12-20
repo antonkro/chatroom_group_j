70d07daf6aeab406c95cd32a79af1d708dbf3ba3
Anton Kroisant <anton.kroisant@student.reutlingen-university.de>
2016-12-20 15:37:54 +0100

    adjust cpu to cloudant max queries

--- app.js
+++ app.js
@@ -59,7 +59,8 @@ app.set('views', __dirname + '/views');
 // scale out ======================================================================
 // vertically
 var cluster = require("cluster");
-var num_processes = require("os").cpus().length/4;
+var num_processes = 2;
+// require("os").cpus().length/4;
 console.log(num_processes);
 // horizontally
 var redis_port = 12889;
