2528d891420b4bce2c4023e027f9d3b63ea60661
Anton Kroisant <anton.kroisant@student.reutlingen-university.de>
2016-12-23 09:04:08 +0100

    redis added

--- routes.js
+++ routes.js
@@ -30,7 +30,7 @@ module.exports = function(app,upload,appEnv) {
         res.render('chatroom', {
             user: req.session.user,
             chatroom: req.session.chatroom,
-            start:appEnv.start
+            instance_index:appEnv.instance_index
 
         });
         // res.sendFile(__dirname + '/views/chatroom.html');
