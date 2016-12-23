1655f3f78a8c1689d5affd24b86fba37e49012d5
Anton Kroisant <anton.kroisant@student.reutlingen-university.de>
2016-12-23 08:57:48 +0100

    start  time

--- routes.js
+++ routes.js
@@ -30,7 +30,7 @@ module.exports = function(app,upload,appEnv) {
         res.render('chatroom', {
             user: req.session.user,
             chatroom: req.session.chatroom,
-            instance_index:appEnv.instance_index
+            start:appEnv.start
 
         });
         // res.sendFile(__dirname + '/views/chatroom.html');
